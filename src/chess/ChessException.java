9b63172ea8b65adfba086a205ef1348ee3d6eabc
Luiz Tonon <luiztonon.08@gmail.com>
2022-09-16 16:53:01 -0300

    possible moves of a piece

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,10 +1,15 @@
 package chess;
 
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
 	private static final long serialVersionUID = 1L;
 	
 	public ChessException(String msg) {
 		super(msg);
+	}
+
+	public char[] getMessage() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
